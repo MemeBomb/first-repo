b1b2aa29bcee57722ddac8e4f4f067b6d5c626f5
MemeBomb <fireus111@gmail.com>
2022-11-24 20:04:50 -0600

    adding a user name and password input to a Yorks

--- grass.js
+++ grass.js
@@ -1,8 +1,17 @@
-console.log('touch grass people!');
-
-function measure(height, width) {
-    return height + width; 
+// function for checking if the user input has valid characters
+function user {
+    if (user ==! String && user ==! Number) {
+        console.log('invalid input, please try again!')
+    }
 }
 
-measure(5, 8); 
-
+// function expression for checkingi if the password input has valid characters
+const password = function pass() {
+    if (pass ==! String && pass ==! Number) {
+        console.log('invalid password, please try again!')
+    }
+}
+// function object for logging hello world. 
+const signUp = signUp => {
+    console.log('hello word');
+}
